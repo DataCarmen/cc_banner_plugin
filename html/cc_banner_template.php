ba00c254d49477a90d7994d7250b96303d6b3ed0
= <maricarmenmartinezdelgado@MBP-de-Mari.home>
2024-08-08 20:44:17 +0200

    basic html template

---
+++ html/cc_banner_template.php
@@ -0,0 +1,11 @@
+<div id="cb-overlay" class="cb-hidden">
+    <div id="cb" class="cb-white-background">
+        <b>Si has llegado hasta esta web, me imagino que ya sabes lo de las cookies... </b>
+        <p class="cb-text">Esta web utiliza cookies para recopilar datos de navegación anónimos. </p>
+        <p>Pulsa <a href="https://carmenweb.com/politica-de-cookies/">aquí</a> para consultar la política de cookies.</p>
+        <div class="cb-right-positioned">
+            <button id="cb-accept-btn" class="cb-btn">Aceptar</button>
+            <button id="cb-config-btn" class="cb-btn">Configurar</button>
+            <button id="cb-reject-btn" class="cb-btn">Rechazar</button>
+        </div>
+    </div>
